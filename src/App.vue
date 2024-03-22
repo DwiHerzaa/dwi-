708efa06f79c0953950bdad027fb5b069107ce0e
Dwi Herzaa <dwiherzaa@gmail.com>
2024-03-22 10:36:03 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    hello kelas f
+  </h1>
+
+<Buttonc/>
+</template>
+
+<script setup>
+import Buttonc from "./components/Buttonc.vue"
+</script>
---
+++ src/components/Buttonc.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="btn">
+        Submit
+    </button>
+</template>
+
+<style scoped>
+.btn {
+    background-color: white;
+    color: purple;
+    font-size: 24px;
+}
+</style>
